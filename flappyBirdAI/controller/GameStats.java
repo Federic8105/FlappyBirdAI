aa31006931a767610a1c085b23cbcb666efeb1d0
Federico Sabbatani <fedesaba05@gmail.com>
2025-08-20 23:00:44 +0200

    Delete flappyBirdAI directory

--- flappyBirdAI/controller/GameStats.java
+++
@@ -1,7 +0,0 @@
-package flappyBirdAI.controller;
-
-public class GameStats {
-    public int fps = 0, nGen = 1, nBirds = 0, nTubePassed = 0, nMaxTubePassed = 0, autoSaveThreshold = 50;
-    public double bestLifeTime = 0;
-    public boolean isAutoSaveEnabled = true;
-}
